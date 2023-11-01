344bb82753dbfbad00a541e5bb06f30f7ab419c7
vladyslavbabenk0 <thevladman17@gmail.com>
2023-11-01 17:10:57 +0100

    HW2

---
+++ routes/api/contacts.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const contactsControllers = require("../../controllers/contacts-controllers.js");
+const isEmptyBody = require("../../middlewares/isEmptyBody.js");
+const validateBody = require("../../decorators/validateBody.js");
+const validateUpdateBody = require("../../decorators/validateUpdateBody.js");
+const schemaValidate = require("../../schemas/contact-schema.js");
+const contactAddValidate = validateBody(schemaValidate.contactAddSchema);
+const contactUpdateValidate = validateUpdateBody(
+  schemaValidate.contactUpdateSchema);
+
+  const router = express.Router();
+
+router.get("/", contactsControllers.getAll);
+  
+router.get("/:contactId", contactsControllers.getById);
+  
+router.delete("/:contactId", contactsControllers.deleteById);
+
+router.post("/", isEmptyBody, contactAddValidate, contactsControllers.add);
+
+router.put(
+  "/:contactId",
+  isEmptyBody,
+  contactUpdateValidate,
+  contactsControllers.updateById
+);
+  
+  module.exports = router;
